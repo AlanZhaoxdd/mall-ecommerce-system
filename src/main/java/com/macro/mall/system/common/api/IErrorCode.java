7ebc457442e9ec2cc4d031a14c9db8b4c530dc05
AlanZhao <alan.zhao@example.com>
2025-05-08 20:32:29 +0800

    feat: 添加 IErrorCode 接口，用于统一错误码结构定义

---
+++ src/main/java/com/macro/mall/system/common/api/IErrorCode.java
@@ -0,0 +1,11 @@
+package com.macro.mall.system.common.api;
+
+/**
+ * @description 封装API的错误码
+ * @date 2025/5/8
+ */
+public interface IErrorCode {
+    long getCode();
+
+    String getMessage();
+}
