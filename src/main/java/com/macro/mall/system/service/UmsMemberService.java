50febffe086748057e303f1f9a68972176870c96
AlanZhao <alan.zhao@example.com>
2025-05-09 17:43:42 +0800

    feat: 定义 UmsMemberService 接口，包含验证码生成与校验方法

---
+++ src/main/java/com/macro/mall/system/service/UmsMemberService.java
@@ -0,0 +1,20 @@
+package com.macro.mall.system.service;
+
+import com.macro.mall.system.common.api.CommonResult;
+
+/**
+ * @description 会员管理Service
+ * @date 2025/5/9
+ */
+public interface UmsMemberService {
+
+    /**
+     * 生成验证码
+     */
+    CommonResult generateAuthCode(String telephone);
+
+    /**
+     * 判断验证码和手机号码是否匹配
+     */
+    CommonResult verifyAuthCode(String telephone, String authCode);
+}
