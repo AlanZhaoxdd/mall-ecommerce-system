4e65cf5fafa02a3a5c93889a5fbdfd4b00310098
AlanZhao <alan.zhao@example.com>
2025-05-10 18:19:09 +0800

    feat: 添加 MallSecurityConfig 配置类，实现自定义 UserDetailsService 获取用户信息

---
+++ src/main/java/com/macro/mall/system/config/MallSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.macro.mall.system.config;
+
+import com.macro.mall.system.domain.AdminUserDetails;
+import com.macro.mall.system.service.UmsAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * @description 自定义配置，用于配置如何获取用户信息
+ * @date 2025/5/10
+ */
+@Configuration
+public class MallSecurityConfig {
+
+    @Autowired
+    private UmsAdminService adminService;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //获取登录用户信息
+        return username -> {
+            AdminUserDetails admin = adminService.getAdminByUsername(username);
+            if (admin != null) {
+                return admin;
+            }
+            throw new UsernameNotFoundException("用户名或密码错误");
+        };
+    }
+}
