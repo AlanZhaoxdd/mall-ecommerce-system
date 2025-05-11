780a853ba255447d17522e1b67207febc1f8b18d
AlanZhao <alan.zhao@example.com>
2025-05-11 19:00:00 +0800

    feat: 新增 EsProductDao 接口，自定义商品数据查询用于同步至 Elasticsearch

--- src/main/java/com/macro/mall/system/dao/EsProductDao.java
+++ src/main/java/com/macro/mall/system/dao/EsProductDao.java
@@ -1,2 +1,14 @@
-package com.macro.mall.system.dao;public class EsProductDap {
+package com.macro.mall.system.dao;
+
+import com.macro.mall.system.nosql.elasticsearch.document.EsProduct;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @description 搜索系统中的商品管理自定义Dao
+ * @date 2025/5/11
+ */
+public interface EsProductDao {
+    List<EsProduct> getAllEsProductList(@Param("id") Long id);
 }
